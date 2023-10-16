2fe9598b4c1f10e9eeef2392ddda013855c87355
RodrigoERuiz <122758239+RodrigoERuiz@users.noreply.github.com>
2023-10-16 11:03:01 -0300

    Update tp_contador1.ino
    Segunda parte TP Contador
    Se agrego sensor de temperatura, motor cc
    y division en subtareas del en el censador de cada botón y en la comprobacion del modo actual

--- tp_contador1.ino
+++ tp_contador1.ino
@@ -6,17 +6,26 @@
 #define f 9
 #define g 8
 
+#define giroDer A2
+#define giroIzq A3
+
+#define sensorTemp A4
+
 #define DISPLAY_UNIDADES A0
 #define DISPLAY_DECENAS A1
 
 #define PULSADOR_INCREMENTAR 5 // Pin del pulsador para aumentar el contador
 #define PULSADOR_DECREMENTAR 3 // Pin del pulsador para disminuir el contador
 #define PULSADOR_REINICIAR 4   // Pin del pulsador para reiniciar el contador a cero
+#define INTERRUPTOR 2 // Pin del interruptor deslizante
 
 int contador = 0;
 int ultimoEstadoIncrementar = HIGH;
 int ultimoEstadoDecrementar = HIGH;
 int ultimoEstadoReiniciar = HIGH;
+int ultimoEstadoInterruptor = HIGH;
+
+bool modoNumerosPrimos = false; // Variable para controlar el modo "Números Primos"
 
 void setup() {
   for (int i = 6; i < 14; i++) {
@@ -27,41 +36,35 @@ void setup() {
   pinMode(PULSADOR_INCREMENTAR, INPUT_PULLUP); 
   pinMode(PULSADOR_DECREMENTAR, INPUT_PULLUP); 
   pinMode(PULSADOR_REINICIAR, INPUT_PULLUP);
-  apagarSegmentos(); 
+  pinMode(INTERRUPTOR, INPUT_PULLUP);
+  pinMode(giroDer, OUTPUT);
+  pinMode(giroIzq, OUTPUT);
+  digitalWrite(giroIzq,LOW);//declaro uno de los pines del motor en LOW para hacer pruebas en refactorizacion
 }
 
+
 void loop() {
-  int estadoIncrementar = digitalRead(PULSADOR_INCREMENTAR);//estado actual del pulsador a cada vuelta
-  int estadoDecrementar = digitalRead(PULSADOR_DECREMENTAR);//estado actual del pulsador a cada vuelta
-  int estadoReiniciar = digitalRead(PULSADOR_REINICIAR);	//estado actual del pulsador a cada vuelta
-
-  if (estadoIncrementar == LOW && ultimoEstadoIncrementar == HIGH) {
-    contador++;
-    if (contador > 99) {
-      contador = 0;
-    }
-  }
-
-  if (estadoDecrementar == LOW && ultimoEstadoDecrementar == HIGH) {
-    contador--;
-    if (contador < 0) {
-      contador = 99;
-    }
-  }
-
-  if (estadoReiniciar == LOW && ultimoEstadoReiniciar == HIGH) {
-    contador = 0;
-  }
+  int estadoIncrementar = digitalRead(PULSADOR_INCREMENTAR);
+  int estadoDecrementar = digitalRead(PULSADOR_DECREMENTAR);
+  int estadoReiniciar = digitalRead(PULSADOR_REINICIAR);
+  int estadoInterruptor = digitalRead(INTERRUPTOR);
+  int temperaturaActual = map(analogRead(sensorTemp), 0, 1023, -50, 450);
   
-//El estado anterior del pulsador pasa a tomar el estado actual antes hacer el siguiente loop
-  ultimoEstadoIncrementar = estadoIncrementar; 
+  controlarTemperatura(temperaturaActual,giroDer);
+  modoNumerosPrimos = verificarModo(estadoInterruptor);
+
+  contador = controlarBotonIncremento(estadoIncrementar,ultimoEstadoIncrementar,modoNumerosPrimos,contador);
+  contador = controlarBotonDecremento(estadoDecrementar,ultimoEstadoDecrementar,modoNumerosPrimos,contador);
+  contador = controlarBotonReiniciar(estadoReiniciar,ultimoEstadoReiniciar,contador);
+
+  ultimoEstadoIncrementar = estadoIncrementar;
   ultimoEstadoDecrementar = estadoDecrementar;
   ultimoEstadoReiniciar = estadoReiniciar;
 
-  mostrarNumero(contador % 10, DISPLAY_UNIDADES); // Para quedarme con la unidad
-  delay(5); 
-  mostrarNumero(contador / 10, DISPLAY_DECENAS); // Para quedarme con la decena
-  delay(5); 
+  mostrarNumero(contador % 10, DISPLAY_UNIDADES);
+  delay(5);
+  mostrarNumero(contador / 10, DISPLAY_DECENAS);
+  delay(5);
 }
 
 void apagarSegmentos() {
@@ -73,8 +76,6 @@ void apagarSegmentos() {
   digitalWrite(f, LOW);
   digitalWrite(g, LOW);
 }
-
-
 
 void mostrarNumero(int numero, int display) 
 {
@@ -91,7 +92,6 @@ void mostrarNumero(int numero, int display)
     digitalWrite(DISPLAY_UNIDADES, LOW);
     digitalWrite(DISPLAY_DECENAS, HIGH);
   }
-
 
   switch (numero) {
     case 0:
@@ -187,5 +187,111 @@ void mostrarNumero(int numero, int display)
   }
 }
 
+bool esPrimo(int n) {
+  if (n <= 1) return false;
+  if (n <= 3) return true;
+  if (n % 2 == 0 || n % 3 == 0) return false;
 
+  for (int i = 5; i * i <= n; i += 6) {
+    if (n % i == 0 || n % (i + 2) == 0) return false;
+  }
+  return true;
+}
 
+int siguienteNumeroPrimo(int numero) {
+  // Esta función encuentra el siguiente número primo mayor que el número dado.
+  // Se utiliza en el modo "Números Primos."
+  int siguiente = numero + 1;
+  while (!esPrimo(siguiente)) {
+    siguiente++;
+  }
+  return siguiente;
+}
+
+int numeroPrimoAnterior(int numero) {
+  // Esta función encuentra el número primo anterior al número dado.
+  // Se utiliza en el modo "Números Primos."
+  if (numero <= 2) return -1; // No hay números primos antes de 2.
+  int anterior = numero - 1;
+  while (anterior >= 2 && !esPrimo(anterior)) {
+    anterior--;
+  }
+  return anterior;
+}
+
+void controlarTemperatura(int temperatura,int pinMotor)
+{
+  if (temperatura > 30)
+  {
+    digitalWrite(pinMotor,HIGH);
+  }
+  else
+  {
+    digitalWrite(pinMotor,LOW);
+  }
+}
+
+bool verificarModo(int interruptorDeslizable)
+{
+  int estadoInterruptor = digitalRead(interruptorDeslizable);
+  if (estadoInterruptor == LOW) {
+    // Cambiar al modo "Números Primos" si el interruptor está en la posición "0"
+    modoNumerosPrimos = true;
+  } else {
+    // Cambiar al modo "Contador" si el interruptor está en la posición "1"
+    modoNumerosPrimos = false;
+  }
+  return modoNumerosPrimos;
+}
+
+int controlarBotonIncremento(int estadoBotonActual,int ultimoEstadoBoton, bool modoNumerosPrimos, int contador)
+{
+  if (estadoBotonActual == LOW && ultimoEstadoBoton == HIGH && modoNumerosPrimos)
+  {
+      contador = siguienteNumeroPrimo(contador);
+      if (contador == 0 || contador > 97) 
+      {
+        // Si volvemos a 0 o superamos 97, mostrar "00" y cambiar de nuevo a "Contador"
+        contador = 2;
+      }
+  }
+    else if(estadoBotonActual == LOW && ultimoEstadoBoton == HIGH && modoNumerosPrimos == false) 
+    {
+      // Modo "Contador"
+      contador++;
+      if (contador > 99) 
+      {
+        contador = 0;
+      }
+    }
+  return contador;
+}
+
+int controlarBotonDecremento(int estadoBotonActual,int ultimoEstadoBoton, bool modoNumerosPrimos, int contador)
+{
+  if (estadoBotonActual == LOW && ultimoEstadoBoton == HIGH && modoNumerosPrimos)
+  {
+      contador = numeroPrimoAnterior(contador);
+      if (contador < 2)
+      {
+        contador = 97;
+      }
+  }
+    else if(estadoBotonActual == LOW && ultimoEstadoBoton == HIGH && modoNumerosPrimos == false) 
+    {
+      // Modo "Contador"
+      contador--;
+      if (contador < 0) {
+        contador = 99;
+      }
+    }
+  return contador;
+}
+
+int controlarBotonReiniciar(int estadoBotonActual,int ultimoEstadoBoton, int contador)
+{
+  if (estadoBotonActual == LOW && ultimoEstadoBoton == HIGH) {
+    contador = 0;
+  }
+  return contador;
+}
